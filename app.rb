4e71e513b83894bf986a7793307d0bd8cb64efa4
Dana Merrick <dmerrick@brightcove.com>
2013-09-04 14:16:25 -0700

    Various improvements.

--- app.rb
+++ app.rb
@@ -3,18 +3,19 @@ require 'sinatra/base'
 require 'sinatra/twitter-bootstrap'
 require 'haml'
 
-class SinatraBootstrap < Sinatra::Base
+class Soupstraw < Sinatra::Base
   require './helpers/render_partial'
 
   register Sinatra::Twitter::Bootstrap::Assets
 
   get '/' do
+    @title = "Soupstraw!"
     haml :index
   end
 
   get '/bitcoins' do
     @title = "Bitcoin Earnings"
-    @string = `/Users/dmerrick/.rvm/rubies/ruby-2.0.0-p247/bin/ruby /Users/dmerrick/other_projects/bitcoin_earnings/bitcoin_earnings.rb`
+    @command_output = `/Users/dmerrick/.rvm/rubies/ruby-2.0.0-p247/bin/ruby /Users/dmerrick/other_projects/bitcoin_earnings/bitcoin_earnings.rb`
     haml :bitcoins
   end
 
--- tasks/application.rake
+++ tasks/application.rake
@@ -2,3 +2,7 @@ desc 'Run the app'
 task :s do
   system "rackup -p 4567"
 end
+
+task :s9393 do
+  system "rackup -p 9393"
+end
