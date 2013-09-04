423f890e58aa04b10f181a3311f04c43688d52d4
Dana Merrick <dmerrick@brightcove.com>
2013-09-04 12:05:14 -0700

    Working on the bitcoin page.

--- app.rb
+++ app.rb
@@ -8,16 +8,15 @@ class SinatraBootstrap < Sinatra::Base
 
   register Sinatra::Twitter::Bootstrap::Assets
 
-  # ?
-  #enable :inline_templates
-
   get '/' do
     haml :index
   end
 
-  get '/btc' do
-    `/Users/dmerrick/bin/bitcoin_earnings.rb`
+  get '/bitcoins' do
+    @string = "---------- 16:46 9/3/2013 ----------\nApproximate BTC mined: 0.01617622\nUSD rate (current avg.): $136.77\n------------------------------------\nProgress to positive ROI: 3.66%\nBTC left to positive ROI: 0.42542378\n------------------------------------\nApproximate USD earned: $2.21\n------------------------------------"
+    haml :bitcoins
   end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
