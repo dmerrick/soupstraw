6db6d12204696ec3578dc7a11d4b066f1bb694a4
Dana Merrick <dmerrick@brightcove.com>
2013-09-14 15:35:49 -0700

    Using the updated unicorn.rb
    Adapted from the chef version.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,23 +1,32 @@
 # config/unicorn.rb
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout 15
+
+pidfile = "tmp/pids/unicorn.pid"
+pid pidfile
+
+timeout 30
+
+stderr_path "log/unicorn.stderr.log"
+stdout_path "log/unicorn.stdout.log"
+
 preload_app true
 
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
+  old_pid = "#{pidfile}.oldbin"
+  if File.exists?(old_pid) and server.pid != old_pid
+    begin
+      Process.kill('QUIT', File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection_handler.clear_all_connections!
+  end
+
 end
 
 after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
 end
