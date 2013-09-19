2775db15c1d0f144f02a1409885e4ed5c489d13c
Dana Merrick <dmerrick@brightcove.com>
2013-09-19 14:42:35 -0400

    WIP: making stats update automagically.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,38 @@
 $(document).ready(function() {
 
-	// Open external links in a new window
-	hostname = window.location.hostname
-	$("a[href^=http]")
-	  .not("a[href*='" + hostname + "']")
-	  .addClass('link external')
-	  .attr('target', '_blank');
+  // open external links in a new window
+  hostname = window.location.hostname
+  $("a[href^=http]")
+    .not("a[href*='" + hostname + "']")
+    .addClass('link external')
+    .attr('target', '_blank');
+
+
+  /* this could be useful later (maybe)
+  $.ajaxSetup({
+    cache: false,
+    beforeSend: function() {
+      $('#content').hide();
+      $('#ajaxy-refresh').show();
+    },
+    complete: function() {
+      $('#loading').hide();
+      $('#ajaxy-refresh').show();
+    },
+    success: function() {
+      $('#loading').hide();
+      $('#ajaxy-refresh').show();
+    }
+  });
+  */
+
+  // this is just a silly proof-of-concept
+  var container = $('#ajaxy-refresh');
+  var randomnumber = Math.floor(Math.random() * 100);
+  container.text('Random Number = ' + randomnumber);
+  var refreshId = setInterval(function() {
+    var randomnumber = Math.floor(Math.random() * 100);
+    container.text('Random Number = ' + randomnumber);
+  }, 3000);
 
 });
