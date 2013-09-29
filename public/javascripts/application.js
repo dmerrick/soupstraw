de00606acb9c7a773bb0acca5b4413b956a555dd
Dana Merrick <dmerrick@brightcove.com>
2013-09-28 18:31:48 -0700

    Moving the income over time graph to the earnings page.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,32 +7,4 @@ $(document).ready(function() {
     .addClass('link external')
     .attr('target', '_blank');
 
-
-  /* this could be useful later (maybe)
-  $.ajaxSetup({
-    cache: false,
-    beforeSend: function() {
-      $('#content').hide();
-      $('#ajaxy-refresh').show();
-    },
-    complete: function() {
-      $('#loading').hide();
-      $('#ajaxy-refresh').show();
-    },
-    success: function() {
-      $('#loading').hide();
-      $('#ajaxy-refresh').show();
-    }
-  });
-  */
-
-  // this is just a silly proof-of-concept
-  var container = $('#ajaxy-refresh');
-  var randomnumber = Math.floor(Math.random() * 100);
-  container.text('Random Number = ' + randomnumber);
-  var refreshId = setInterval(function() {
-    var randomnumber = Math.floor(Math.random() * 100);
-    container.text('Random Number = ' + randomnumber);
-  }, 3000);
-
 });
