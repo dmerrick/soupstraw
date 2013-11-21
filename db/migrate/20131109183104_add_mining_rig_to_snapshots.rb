dca8d1f6e9a41e2f023d5284160bd8f7088ba450
Dana Merrick <dmerrick@brightcove.com>
2013-11-21 11:10:17 -0500

    Very minor whitespace fixes.

--- db/migrate/20131109183104_add_mining_rig_to_snapshots.rb
+++ db/migrate/20131109183104_add_mining_rig_to_snapshots.rb
@@ -1,13 +1,13 @@
 class AddMiningRigToSnapshots < ActiveRecord::Migration
   def up
-  	change_table :bitcoin_stats_snapshots do |t|
-	  t.belongs_to :mining_rig
-	end
+    change_table :bitcoin_stats_snapshots do |t|
+      t.belongs_to :mining_rig
+    end
   end
 
   def down
-  	 change_table :bitcoin_stats_snapshots do |t|
-	  t.remove :mining_rig_id
-	end
+    change_table :bitcoin_stats_snapshots do |t|
+      t.remove :mining_rig_id
+    end
   end
 end
