e3ff6b48d32cccfb2b95c62ef437a912b0ce0872
Dana Merrick <dmerrick@brightcove.com>
2013-11-21 09:26:57 -0500

    Making the Bitcoin helper more moduley.

--- helpers/bitcoin.rb
+++ helpers/bitcoin.rb
@@ -1,19 +1,24 @@
 module Bitcoin
 
-  def self.usd_value_url
+  def usd_value_url
     'http://api.bitcoinaverage.com/no-mtgox/ticker/USD'
   end
 
   # find current BTC->USD exchange rate (weighted average)
-  def self.current_usd_value
+  def current_usd_value
     current_value_json = JSON.parse(URI.parse(usd_value_url).read)
     current_value_json['last'].to_f
   end
 
-  def self.usd_format(value)
+  def usd_format(value)
     string = value.round(2).to_s
     string += '0' if total.split('.').last.size != 2
     string
   end
+
+  # allow Bitcoin.foo() calls
+  module_function :usd_value_url
+  module_function :current_usd_value
+  module_function :usd_format
 
 end
