2784a65d47f7491575328bea9530cbffa0668a75
Dana Merrick <dmerrick@brightcove.com>
2013-11-28 12:45:33 -0800

    Tweaking the logic on summing rig costs.

--- models/mining_rig.rb
+++ models/mining_rig.rb
@@ -93,8 +93,9 @@ class MiningRig < ActiveRecord::Base
   def +(other_rig)
     MiningRig.new do |rig|
       rig.name = "Composite of MiningRig##{id} and MiningRig##{other_rig.id}"
-      rig.btc_cost = btc_cost.to_f + other_rig.btc_cost.to_f
-      rig.usd_cost = usd_cost.to_f + other_rig.usd_cost.to_f
+
+      rig.btc_cost = btc_cost + other_rig.btc_cost if btc_cost && other_rig.btc_cost
+      rig.usd_cost = usd_cost + other_rig.usd_cost if usd_cost && other_rig.usd_cost
 
       # use the first rig's settings:
       #FIXME: use something else here?
