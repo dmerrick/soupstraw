5b911bd2ae1d29c583fb0d4785a0f39cc09b3eb4
Dana Merrick <dmerrick@brightcove.com>
2013-11-28 01:45:26 -0800

    Adding a lights route and better on/off routes.

--- routes/lights.rb
+++ routes/lights.rb
@@ -1,15 +1,23 @@
 class Soupstraw < Sinatra::Base
 
+  get '/lights', auth: :user do
+    @title = 'Light Controls'
+    response = home_api('/lights')
+    @lights = JSON.parse(response.body)
+    @json_data = JSON.pretty_generate(@lights) if request[:with_json]
+    haml :lights
+  end
+
   get '/lights/on', auth: :user do
     response = home_api('/lights/on')
-    @content = response.body
-    haml :blank
+    #TODO: make this actually take advantage of the response
+    redirect '/lights', info: 'Lights are now on.'
   end
 
   get '/lights/off', auth: :user do
     response = home_api('/lights/off')
-    @content = response.body
-    haml :blank
+    #TODO: make this actually take advantage of the response
+    redirect '/lights', info: 'Lights are now off.'
   end
 
 end
