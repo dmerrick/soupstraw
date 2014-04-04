fae9eb1d36d44992e2c7a58a49cddfc6729a1ad7
Dana Merrick <dmerrick@brightcove.com>
2014-04-03 23:08:08 -0700

    Adding support for pages that refresh themselves.

--- app/routes/wall_remote.rb
+++ app/routes/wall_remote.rb
@@ -3,6 +3,7 @@ class Soupstraw < Sinatra::Base
   get '/wall_remote', auth: :user do
     @title = 'Soupstraw Wall Remote!'
     @no_navbar = true
+    @refresh = 30.minutes
     haml :wall_remote
   end
 
