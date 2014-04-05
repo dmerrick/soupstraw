5f69da7c3b324aa8b30fa6fc3472129e347f2dc6
Dana Merrick <dmerrick@brightcove.com>
2014-04-04 21:13:37 -0700

    WIP: adding play/pause routes.

--- app/routes/wall_remote.rb
+++ app/routes/wall_remote.rb
@@ -7,4 +7,22 @@ class Soupstraw < Sinatra::Base
     haml :wall_remote
   end
 
+  get '/wall_remote/play', auth: :user do
+    @title = 'Soupstraw Wall Remote!'
+    @no_navbar = true
+    @refresh = 30.minutes
+
+    home_api('/play')
+    haml :wall_remote
+  end
+
+  get '/wall_remote/pause', auth: :user do
+    @title = 'Soupstraw Wall Remote!'
+    @no_navbar = true
+    @refresh = 30.minutes
+
+    home_api('/pause')
+    haml :wall_remote
+  end
+
 end
