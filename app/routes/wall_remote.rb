ec89a392038ef05c884474546620cfecc5a1c57c
Dana Merrick <dmerrick@brightcove.com>
2014-05-04 20:36:45 -0700

    Updating media center routes.

--- app/routes/wall_remote.rb
+++ app/routes/wall_remote.rb
@@ -5,45 +5,9 @@ class Soupstraw < Sinatra::Base
     haml :wall_remote
   end
 
-  get '/wall_remote/play', auth: :user do
+  get '/wall_remote/:command', auth: :user do
     wall_remote_settings
-    response = home_api('/itunes/play')
-    haml :wall_remote
-  end
-
-  get '/wall_remote/pause', auth: :user do
-    wall_remote_settings
-    response = home_api('/itunes/pause')
-    haml :wall_remote
-  end
-
-  get '/wall_remote/stop', auth: :user do
-    wall_remote_settings
-    response = home_api('/itunes/stop')
-    haml :wall_remote
-  end
-
-  get '/wall_remote/next', auth: :user do
-    wall_remote_settings
-    response = home_api('/itunes/next')
-    haml :wall_remote
-  end
-
-  get '/wall_remote/previous', auth: :user do
-    wall_remote_settings
-    response = home_api('/itunes/previous')
-    haml :wall_remote
-  end
-
-  get '/wall_remote/volume/up', auth: :user do
-    wall_remote_settings
-    response = home_api('/itunes/volume/up')
-    haml :wall_remote
-  end
-
-  get '/wall_remote/volume/down', auth: :user do
-    wall_remote_settings
-    response = home_api('/itunes/volume/down')
+    response = home_api("/wall_remote/#{params[:command]}")
     haml :wall_remote
   end
 
